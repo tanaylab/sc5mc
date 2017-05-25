dfa28fee57c96a2ba0c67602c9ef48fa29a5cf52
Aviezer Lifshitz <aviezer.lifshitz@weizmann.ac.il>
2017-05-26 00:15:52 +0300

    added documentation. added functions to cluster cpgs. some cosmetics.

--- R/cluster_cells.R
+++ R/cluster_cells.R
@@ -1,83 +1,193 @@
+
+#' Cluster cells
+#' 
+#' @param clustering_distance distance measure used for clustering the correlations
+#' @param clustering_method  clustering method used for clustering the correlations. Accepts the same values as ‘hclust’.
+#' @param plot plot the clustered correlation matrix
+#' @param ... additional parameters for sc5mc.plot_cor_mat
+#' @inheritParams sc5mc.calc_pdiff
+#' @inheritParams sc5mc.calc_pdiff_cor
+#' @inheritParams sc5mc.plot_cor_mat
+#' 
+#' @return list with the following components: 
+#' 'cell_comp' - pairwise comparison of cell CpGs.
+#' 'cell cor' - pairwise cell correlations.
+#' 'mat' - correlation matrix.
+#' 'hclust_rows' - hclust object used to cluster the rows.
+#' 'hclust_cols' - hclust object used to cluster the columns.
+#' If plot is TRUE the list is returned invisibly
+#' 
+#' @export
+sc5mc.cluster_cells <- function(smat, min_cells=10, min_cgs=100, use='pairwise.complete.obs', method='spearman', clustering_distance = 'euclidean',  clustering_method = 'ward.D2', intervs=NULL, cols=NULL, samp_data=NULL, plot=FALSE, ...){
+    message('calculating pairwise statistics of cells...')
+    cell_comp <- sc5mc.calc_pdiff(smat, min_cgs=min_cgs, intervs=intervs, cols=cols, samp_data=samp_data)
+
+    message('calculating pairwise correlations...')
+    cell_cor <- sc5mc.calc_pdiff_cor(cell_comp, min_cells=min_cells, use=use, method=method)
+    mat <- cell_cor %>% filter(cell1 != cell2) %>% spread(cell2, corr) %>% select(-cell1) %>% as.matrix()
+
+    message('clustering...')
+    hclust_cols <- dist(t(mat), method = clustering_distance) %>% hclust(method = clustering_method) %>%  dendsort::dendsort()
+    hclust_rows <- dist(mat, method = clustering_distance) %>% hclust(method = clustering_method) %>%  dendsort::dendsort()
+
+    res <- list(cell_comp = cell_comp,
+                cell_cor = cell_cor, 
+                mat = mat, 
+                hclust_rows = hclust_rows, 
+                hclust_cols = hclust_cols)
+
+    if (plot){
+        sc5mc.plot_cor_mat(cell_cor, cluster_rows=hclust_rows, cluster_cols=hclust_cols, ...)
+        invisible(res)
+    } else {
+        return(res)    
+    }    
+}
+
+#' calculate pairwise statistics of cells
+#'
+#' @param smat smat object
+#'
+#' @param min_cgs minimal number of cpgs covered in both cells
+#' @param intervs intervals set to filter smat by before calculating. if NULL
+#' all CpGs would be used.
+#' @param cols vector of names of cells to use. If NULL all cells would be used.
+#' @param samp_data data frame with additional annotation of the cell.
+#' must have 'track' field with the cell name.
+#'
 #' @export
 sc5mc.calc_pdiff <- function(smat, min_cgs=100, intervs=NULL, cols=NULL, samp_data=NULL){
     if (!is.null(intervs) || !is.null(cols)){
-        smat <- smat.filter(smat, intervs=intervs, cols=cols)    
-    }       
+        smat <- smat.filter(smat, intervs=intervs, cols=cols)
+    }
 
     calc_mat_stat <- function(smat, mat1_name, mat2_name, column_name){
         if (mat1_name == mat2_name){
-            stat <- crossprod(smat[[mat1_name]])         
+            stat <- crossprod(smat[[mat1_name]])
         } else {
-            stat <- crossprod(smat[[mat1_name]], smat[[mat2_name]])             
-        }        
+            stat <- crossprod(smat[[mat1_name]], smat[[mat2_name]])
+        }
         stat <- stat %>% as.matrix() %>% reshape2::melt() %>% set_names(c('cell1', 'cell2', column_name))
         return(stat)
     }
 
     stats_tab <- tribble(
-       ~mat1, ~mat2, ~col,
+       ~mat1,    ~mat2,        ~col,
        'meth',   'meth',       'n11',
        'unmeth', 'unmeth',     'n00',
        'meth',   'unmeth',     'n10',
-       'unmeth', 'meth',       'n01',       
-       'cov',    'cov',        'ntot'
-     )
+       'unmeth', 'meth',       'n01',
+       'cov',    'cov',        'ntot' )
 
     cell_comp <- plyr::alply(stats_tab, 1, function(x) calc_mat_stat(smat, x$mat1, x$mat2, x$col), .parallel=T)
 
     cell_comp <- cell_comp %>%  reduce(left_join, by=c('cell1', 'cell2')) %>% tbl_df
 
-    cell_comp <- cell_comp %>% 
-        mutate(n = n11 + n00 + n10 + n01, psame = (n00 + n11) / n, pdiff=1 - psame) %>% 
-        filter(cell1 != cell2, n >= min_cgs)  
-        
+    cell_comp <- cell_comp %>%
+        mutate(n = n11 + n00 + n10 + n01, psame = (n00 + n11) / n, pdiff=1 - psame) %>%
+        filter(cell1 != cell2, n >= min_cgs)
+
     if (!is.null(samp_data)){
         cell_comp <- cell_comp %>% left_join(samp_data %>% select(cell1 = track, lib), by='cell1') %>% mutate(cell1 = lib) %>% select(-lib)
-        cell_comp <- cell_comp  %>% left_join(samp_data %>% select(cell2 = track, lib), by='cell2') %>% mutate(cell2 = lib) %>% select(-lib)        
+        cell_comp <- cell_comp  %>% left_join(samp_data %>% select(cell2 = track, lib), by='cell2') %>% mutate(cell2 = lib) %>% select(-lib)
     }
-    
+
     return(cell_comp)
 }
 
+#' Calculate pairwise correlation of cells pdiff
+#' 
+#' @param cell_comp Output of \code{sc5mc.calc_pdiff}: data frame with pairs of cells (cell1, cell2) and 'pdiff' column with the fraction of different CpGs.
+#'
+#' @param min_cells minimal number of cells with pdiff statistic in both cells
+#' @param use `use` parameter of `cor` function
+#' @param method `method` parameter of `cor` function
+#' @param cells vector with cell names of cells to operate on. If NULL calculation would be 
+#' done on all cells.
+#' 
+#'
+#' @export
+sc5mc.calc_pdiff_cor <- function(cell_comp, min_cells, use='pairwise.complete.obs', method='spearman', cells=NULL){
+    cell_comp <- cell_comp %>% select(cell1, cell2, pdiff)
 
+    if (!is.null(cells)){
+        cell_comp <- cell_comp %>% filter(cell1 %in% cells, cell2 %in% cells)
+    }
+
+    good_cells <- cell_comp %>%
+        group_by(cell1) %>%
+        mutate(val_num = n()) %>%
+        filter(val_num >= min_cells) %>%
+        .$cell1 %>%
+        unique()
+
+    cell_comp <- cell_comp %>%
+        filter(cell1 %in% good_cells, cell2 %in% good_cells)
+
+    cell_comp <- cell_comp  %>%
+        spread(cell2, pdiff) %>%
+        .[,-1] %>%
+        as.matrix()
+
+    cell_cor <- cor(cell_comp, use=use, method=method) %>%
+        reshape2::melt() %>%
+        rename(cell1=Var1, cell2=Var2, corr=value) %>%
+        tbl_df
+    return(cell_cor)
+}
+
+
+#' @param smat
+#'
+#' @param min_cells
+#' @param intervs
+#' @param cols
+#'
 #' @export
 sc5mc.calc_pdiff_rows <- function(smat, min_cells=5, intervs=NULL, cols=NULL){
     if (!is.null(intervs) || !is.null(cols)){
-        smat <- smat.filter(smat, intervs=intervs, cols=cols)    
-    }       
+        smat <- smat.filter(smat, intervs=intervs, cols=cols)
+    }
 
     calc_mat_stat <- function(smat, mat1_name, mat2_name, column_name){
         if (mat1_name == mat2_name){
-            stat <- tcrossprod(smat[[mat1_name]])         
+            stat <- tcrossprod(smat[[mat1_name]])
         } else {
-            stat <- tcrossprod(smat[[mat1_name]], smat[[mat2_name]])             
-        }        
+            stat <- tcrossprod(smat[[mat1_name]], smat[[mat2_name]])
+        }
         stat <- stat %>% as.matrix() %>% reshape2::melt() %>% set_names(c('interval1', 'interval2', column_name))
         return(stat)
     }
 
     stats_tab <- tribble(
-       ~mat1, ~mat2, ~col,
-       'meth',   'meth',       'n11',
-       'unmeth', 'unmeth',     'n00',
-       'meth',   'unmeth',     'n10',
-       'unmeth', 'meth',       'n01',       
-       'cov',    'cov',        'ntot'
-     )
+        ~mat1, ~mat2, ~col,
+        'meth',   'meth',       'n11',
+        'unmeth', 'unmeth',     'n00',
+        'meth',   'unmeth',     'n10',
+        'unmeth', 'meth',       'n01',
+        'cov',    'cov',        'ntot'
+    )
 
     interval_comp <- plyr::alply(stats_tab, 1, function(x) calc_mat_stat(smat, x$mat1, x$mat2, x$col), .parallel=T)
 
 
     interval_comp <- interval_comp %>%  reduce(left_join, by=c('interval1', 'interval2')) %>% tbl_df
 
-    interval_comp <- interval_comp %>% 
-        mutate(n = n11 + n00 + n10 + n01, psame = (n00 + n11) / n, pdiff=1 - psame) %>% 
-        filter(interval1 != interval2, n >= min_cells)  
-   
-    
+    interval_comp <- interval_comp %>%
+        mutate(n = n11 + n00 + n10 + n01, psame = (n00 + n11) / n, pdiff=1 - psame) %>%
+        filter(interval1 != interval2, n >= min_cells)
+
+
     return(interval_comp)
 }
 
+#' @param row_comp
+#'
+#' @param min_intervals
+#' @param use
+#' @param method
+#' @param intervals
+#'
 #' @export
 sc5mcs.calc_pdiff_cor_rows <- function(row_comp, min_intervals, use='pairwise.complete.obs', method='spearman', intervals=NULL){
     row_comp <- row_comp %>% rename(cell1 = interval1, cell2 = interval2)
@@ -86,43 +196,34 @@ sc5mcs.calc_pdiff_cor_rows <- function(row_comp, min_intervals, use='pairwise.co
     return(row_cor)
 }
 
+
+#' @param cell_cor
+#'
+#' @param min_vals_row
+#' @param min_vals_col
+#' @param row_ord
+#' @param col_ord
+#' @param show_colnames
+#' @param show_rownames
+#' @param breaks
+#' @param color_pal
+#' @param ...
+#'
 #' @export
-sc5mc.calc_pdiff_cor <- function(cell_comp, min_cells, use='pairwise.complete.obs', method='spearman', cells=NULL){
-    cell_comp <- cell_comp %>% select(cell1, cell2, pdiff)
-
-    if (!is.null(cells)){
-        cell_comp <- cell_comp %>% filter(cell1 %in% cells, cell2 %in% cells)
-    }    
-
-    good_cells <- cell_comp %>% 
-        group_by(cell1) %>% 
-        mutate(val_num = n()) %>% 
-        filter(val_num >= min_cells) %>% 
-        .$cell1 %>%
-        unique()
-
-    cell_comp <- cell_comp %>% 
-        filter(cell1 %in% good_cells, cell2 %in% good_cells)
-
-    cell_comp <- cell_comp  %>%         
-        spread(cell2, pdiff) %>%
-        .[,-1] %>%
-        as.matrix()
-
-    cell_cor <- cor(cell_comp, use=use, method=method) %>% 
-        reshape2::melt() %>% 
-        rename(cell1=Var1, cell2=Var2, corr=value) %>% 
-        tbl_df
-    return(cell_cor)
-}
-
-#' @export
-sc5mc.plot_cor_mat <- function(cell_cor, min_vals_row=100, min_vals_col=100, row_ord=NULL, col_ord=NULL,  show_colnames=FALSE, show_rownames=FALSE, breaks=NULL, color_pal=NULL, ...){
-
+sc5mc.plot_cor_mat <- function(cell_cor,
+                               min_vals_row = 100,
+                               min_vals_col = 100,
+                               row_ord = NULL,
+                               col_ord = NULL,
+                               show_colnames = FALSE,
+                               show_rownames = FALSE,
+                               breaks = NULL,
+                               color_pal = NULL,
+                               ...) {
     if (is.null(color_pal)){
-        color_pal <- c('blue', 'white', '#ffffd4','#fed98e','#fe9929','#d95f0e','#993404', 'black')   
+        color_pal <- c('blue', 'white', '#ffffd4','#fed98e','#fe9929','#d95f0e','#993404', 'black')
         breaks1 <-  quantile(cell_cor[cell_cor$cell1 != cell_cor$cell2, ]$corr, seq(0,1,1/(length(color_pal) - 1)), na.rm=T)
-        pallete <- build_pallette(data.frame(point =breaks1, color =color_pal), 1000)           
+        pallete <- build_pallette(data.frame(point =breaks1, color =color_pal), 1000)
     }
 
     if (is.null(breaks)){
@@ -130,8 +231,8 @@ sc5mc.plot_cor_mat <- function(cell_cor, min_vals_row=100, min_vals_col=100, row
         zlim <- c(min(breaks1), max(breaks1))
         breaks <- seq(zlim[1], zlim[2], length.out=1001)
     }
- 
-    cell_cor <- cell_cor %>% filter(cell1 != cell2) %>% spread(cell2, corr) 
+
+    cell_cor <- cell_cor %>% filter(cell1 != cell2) %>% spread(cell2, corr)
     if (!is.null(row_ord)){
         cell_cor <- cell_cor[row_ord, ]
     }
@@ -140,7 +241,7 @@ sc5mc.plot_cor_mat <- function(cell_cor, min_vals_row=100, min_vals_col=100, row
         cell_cor <- cell_cor[, c(1, col_ord + 1)]
     }
 
-    res <- cell_cor %>% pheatmap1(color=pallete, method='ward.D2', breaks=breaks, show_colnames=show_colnames, show_rownames=show_rownames, ...)   
+    res <- cell_cor %>% pheatmap1(color=pallete, method='ward.D2', breaks=breaks, show_colnames=show_colnames, show_rownames=show_rownames, ...)
     invisible(res)
 }
 
--- R/qc.R
+++ R/qc.R
@@ -1,3 +1,10 @@
+#' @param smat
+#'
+#' @param min_cpgs
+#' @param max_cpgs
+#' @param min_cells
+#' @param max_cells
+#'
 #' @export
 sc5mc.qc_plot <- function(smat, min_cpgs=1, max_cpgs=Inf, min_cells=1, max_cells=Inf){
 	if (any(min_cpgs != 1, max_cpgs != Inf,  min_cells != 1, max_cells != Inf)){
@@ -10,27 +17,35 @@ sc5mc.qc_plot <- function(smat, min_cpgs=1, max_cpgs=Inf, min_cells=1, max_cells
 	return(p)
 }
 
+#' @param smat
+#'
+#' @param type
+#'
 #' @export
 sc5mc.plot_cpg_marginals <- function(smat, type='percent'){
 	mars <- smat.cpg_marginals(smat) %>% filter(cells > 0) %>% select(cells)
 	mars <- mars %>% group_by(cells) %>% summarise(cpgs = n()) %>% arrange(-cells) %>% mutate(c_cpgs = cumsum(cpgs), p = c_cpgs / sum(cpgs))
 	x_breaks <- c(1,2,3,4,10,as.numeric(round(quantile(mars$cells, probs=c(0.05, 0.1, 0.15, 0.2, 0.3, 0.5, 0.7, 0.9, 1))))) %>% unique %>% sort
 	if (type == 'percent'){
-		p <- mars %>% ggplot(aes(x=cells, y=p)) + geom_point(size=0.3) + scale_x_log10(breaks=x_breaks) + scale_y_continuous(label=scales::percent) + ylab('% of CpGs with at least x cells') + xlab('log10(# of cells)')	
+		p <- mars %>% ggplot(aes(x=cells, y=p)) + geom_point(size=0.3) + scale_x_log10(breaks=x_breaks) + scale_y_continuous(label=scales::percent) + ylab('% of CpGs with at least x cells') + xlab('log10(# of cells)')
 	} else {
 		p <- mars %>% ggplot(aes(x=cells, y=c_cpgs)) + geom_point(size=0.3) + scale_x_log10(breaks=x_breaks) + scale_y_log10(label=comify) + ylab('log10(# of CpGs with at least x cells)') + xlab('log10(# of cells)')
 	}
 	p <- p + labs(subtitle = qq('number of cells = @{comify(ncol(smat$cov))}, number of cpgs = @{comify(nrow(smat$cov))}'))
-	return(p)	
+	return(p)
 }
 
+#' @param smat
+#'
+#' @param type
+#'
 #' @export
 sc5mc.plot_cell_marginals <- function(smat, type='percent'){
 	mars <- smat.cell_marginals(smat) %>% filter(cov > 0) %>% select(cov)
 	mars <- mars %>% group_by(cov) %>% summarise(cells = n())  %>% arrange(-cov) %>% mutate(c_cells = cumsum(cells), p = c_cells / sum(cells))
 
 	x_breaks <- as.numeric(round(quantile(mars$cov, probs=c(0.05, 0.1, 0.15, 0.2, 0.3, 0.5, 0.7, 0.9, 1))))
-	
+
 	if (type == 'percent'){
 		p <-mars %>% ggplot(aes(x=cov, y=p)) + geom_point(size=0.3) + scale_x_log10(label=comify, breaks=x_breaks) + scale_y_continuous(label=scales::percent) + ylab('% of cells with at least x CpGs') + xlab('log10(# of CpGs)')
 	} else {
@@ -38,16 +53,18 @@ sc5mc.plot_cell_marginals <- function(smat, type='percent'){
 	}
 
 	p <- p + labs(subtitle = qq('number of cells = @{comify(ncol(smat$cov))}, number of cpgs = @{comify(nrow(smat$cov))}'))
-	return(p)	
+	return(p)
 }
 
+#' @param smat
+#'
 #' @export
-sc5mc.plot_pairs_coverage <- function(smat){	
+sc5mc.plot_pairs_coverage <- function(smat){
 	mars <- smat.cell_pairs_marginals(smat)
 	cmars <- mars %>% count(ntot) %>% arrange(-ntot) %>% mutate(n = n / 2, cn = cumsum(n), p = cn / sum(n))
 
 	x_breaks <- as.numeric(round(quantile(cmars$ntot, probs=c(0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 0.9, 1))))
 	p <- cmars %>% ggplot(aes(x=ntot, y=p)) + geom_point(size=0.3) + xlab('log10(# of common CpGs)') + ylab('% of cell pairs') + scale_x_log10(label=comify, breaks=x_breaks) + scale_y_continuous(labels=scales::percent) + labs(subtitle = qq('number of cells = @{comify(length(unique(mars$cell1)))}, number of pairs = @{comify(nrow(mars) / 2)}'))
 	return(p)
-	
+
 }
