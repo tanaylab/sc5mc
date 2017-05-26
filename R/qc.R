a29ae7e533607b289ea5eb84668e34f199702223
Aviezer Lifshitz <aviezer.lifshitz@weizmann.ac.il>
2017-05-26 12:25:58 +0300

    added cpg pairs marginal calculation and plot

--- R/qc.R
+++ R/qc.R
@@ -1,19 +1,21 @@
-#' @param smat
+#' Plot QC statistics of smat object
+#' 
+#' 
+#' @param cpg_pairs_min_cells minimal number of CpGs to consider in cg_pairs plot
+#' @inheritParams smat.filter_by_cov
 #'
-#' @param min_cpgs
-#' @param max_cpgs
-#' @param min_cells
-#' @param max_cells
-#'
+#' @return plot
+#' 
 #' @export
-sc5mc.qc_plot <- function(smat, min_cpgs=1, max_cpgs=Inf, min_cells=1, max_cells=Inf){
+sc5mc.qc_plot <- function(smat, min_cpgs=1, max_cpgs=Inf, min_cells=1, max_cells=Inf, cpg_pairs_min_cells=20){
 	if (any(min_cpgs != 1, max_cpgs != Inf,  min_cells != 1, max_cells != Inf)){
 		smat <- smat.filter_by_cov(smat, min_cells=min_cells, max_cells=max_cells, max_cpgs=max_cpgs, min_cpgs=min_cpgs)
 	}
 	cgp_mars <- sc5mc.plot_cpg_marginals(smat, type='percent')
 	cell_mars <- sc5mc.plot_cell_marginals(smat, type='percent')
-	pairs_mars <- sc5mc.plot_pairs_coverage(smat)
-	p <- cowplot::plot_grid(cgp_mars, cell_mars, pairs_mars, align='hv', labels=LETTERS[1:3])
+	cell_pairs_mars <- sc5mc.plot_cell_pairs_coverage(smat)
+	cg_pairs_mars <- sc5mc.plot_cg_pairs_coverage(smat, min_cells=cpg_pairs_min_cells)
+	p <- cowplot::plot_grid(cgp_mars, cell_mars, cell_pairs_mars, cg_pairs_mars, align='hv', labels=LETTERS[1:4], ncol=2)
 	return(p)
 }
 
@@ -59,12 +61,28 @@ sc5mc.plot_cell_marginals <- function(smat, type='percent'){
 #' @param smat
 #'
 #' @export
-sc5mc.plot_pairs_coverage <- function(smat){
+sc5mc.plot_cell_pairs_coverage <- function(smat){
 	mars <- smat.cell_pairs_marginals(smat)
-	cmars <- mars %>% count(ntot) %>% arrange(-ntot) %>% mutate(n = n / 2, cn = cumsum(n), p = cn / sum(n))
+	cmars <- mars %>% count(ntot) %>% arrange(-ntot) %>% mutate(cn = cumsum(n), p = cn / sum(n))
 
 	x_breaks <- as.numeric(round(quantile(cmars$ntot, probs=c(0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 0.9, 1))))
-	p <- cmars %>% ggplot(aes(x=ntot, y=p)) + geom_point(size=0.3) + xlab('log10(# of common CpGs)') + ylab('% of cell pairs') + scale_x_log10(label=comify, breaks=x_breaks) + scale_y_continuous(labels=scales::percent) + labs(subtitle = qq('number of cells = @{comify(length(unique(mars$cell1)))}, number of pairs = @{comify(nrow(mars) / 2)}'))
+	p <- cmars %>% ggplot(aes(x=ntot, y=p)) + geom_point(size=0.3) + xlab('log10(# of jointly covered CpGs)') + ylab('% of cell pairs') + scale_x_log10(label=comify, breaks=x_breaks) + scale_y_continuous(labels=scales::percent) + labs(subtitle = qq('number of cells = @{comify(length(unique(mars$cell1)))}, number of pairs = @{comify(nrow(mars))}'))
 	return(p)
-
 }
+
+#' @param smat
+#'
+#' @export
+sc5mc.plot_cg_pairs_coverage <- function(smat, min_cells=30){
+	mars <- smat.cpg_pairs_marginals(smat,  min_cells=min_cells)
+	cmars <- mars %>% arrange(-ncells) %>% mutate(cn = cumsum(npairs), p = cn / sum(npairs))
+
+	ncpgs <- sum(rowSums(smat$cov) >= min_cells)
+
+	x_breaks <- as.numeric(round(quantile(cmars$ncells, probs=c(0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 0.9, 1))))
+	
+	p <- cmars %>% ggplot(aes(x=ncells, y=p)) + geom_point(size=0.3) + xlab('log10(# of jointly covered cells)') + ylab('% of CpG pairs') + scale_x_log10(label=comify, breaks=x_breaks) + scale_y_continuous(labels=scales::percent) + labs(subtitle = qq('number of cpgs = @{comify(ncpgs)} (covered by at least @{min_cells} cells)\nnumber of pairs = @{comify(sum(mars$npairs))}'))
+	
+	return(p)
+}
+
